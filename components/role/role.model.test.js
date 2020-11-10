dbd3b37b20e0a10fd23cd06568949c9bfdc8ad5f
asus <sashmlv@yandex.ru>
2020-11-10 20:08:12 +0300

    Update user test.

--- components/role/role.model.test.js
+++ components/role/role.model.test.js
@@ -1,79 +1,79 @@
 'use strict';
 
 const test = require('ava'),
-  rm = require('./role.model');
+  rom = require('./role.model');
 
 test(`role.model.create`, async t => {
 
-  let data = await rm.create({name: 'name1', record_status: 'active'});
+  let role = await rom.create({name: 'name1', record_status: 'active'});
 
-  t.deepEqual(data.id, 1);
-  t.deepEqual(data.name, 'name1');
-  t.deepEqual(data.record_status, 'active');
+  t.deepEqual(role.id, 1);
+  t.deepEqual(role.name, 'name1');
+  t.deepEqual(role.record_status, 'active');
 
-  data = await rm.create({name: 'name2', record_status: 'active'});
+  role = await rom.create({name: 'name2', record_status: 'active'});
 
-  t.deepEqual(data.id, 2);
-  t.deepEqual(data.name, 'name2');
-  t.deepEqual(data.record_status, 'active');
+  t.deepEqual(role.id, 2);
+  t.deepEqual(role.name, 'name2');
+  t.deepEqual(role.record_status, 'active');
 
-  data = await rm.create({name: 'name3', record_status: 'active'});
+  role = await rom.create({name: 'name3', record_status: 'active'});
 
-  t.deepEqual(data.id, 3);
-  t.deepEqual(data.name, 'name3');
-  t.deepEqual(data.record_status, 'active');
+  t.deepEqual(role.id, 3);
+  t.deepEqual(role.name, 'name3');
+  t.deepEqual(role.record_status, 'active');
 
-  let err = await t.throwsAsync(_=> rm.create({name: 'name3', record_status: 'active'}));
+  let err = await t.throwsAsync(_=> rom.create({name: 'name3', record_status: 'active'}));
   t.deepEqual(err.code, '23505'); // duplicate key
 
-  err = await t.throwsAsync(_=> rm.create({name: 'name3', record_status: 'some'}));
+  err = await t.throwsAsync(_=> rom.create({name: 'name3', record_status: 'some'}));
   t.deepEqual(err.code, '23514'); // failing row contains some
 });
 
 test(`role.model.getRoles`, async t => {
 
-  let data = await rm.getRoles({limit: 10, offset: 0, record_status: 'active'});
+  let role = await rom.getRoles({limit: 10, offset: 0, record_status: 'active'});
 
-  t.deepEqual(data.length, 3);
-  t.deepEqual(data[0].name, 'name1');
-  t.deepEqual(data[1].name, 'name2');
-  t.deepEqual(data[2].name, 'name3');
+  t.deepEqual(role.length, 3);
+  t.deepEqual(role[0].name, 'name1');
+  t.deepEqual(role[1].name, 'name2');
+  t.deepEqual(role[2].name, 'name3');
 
-  data = await rm.getRoles({limit: 1, offset: 1, record_status: 'active'});
+  role = await rom.getRoles({limit: 1, offset: 1, record_status: 'active'});
 
-  t.deepEqual(data.name, 'name2');
+  t.deepEqual(role.name, 'name2');
 });
 
 test(`role.model.getRole`, async t => {
 
-  let data = await rm.getRole({id: 1, record_status: 'active'});
+  let role = await rom.getRole({id: 1, record_status: 'active'});
 
-  t.deepEqual(data.id, 1);
-  t.deepEqual(data.name, 'name1');
+  t.deepEqual(role.id, 1);
+  t.deepEqual(role.name, 'name1');
 });
 
 test(`role.model.update`, async t => {
 
-  let data = await rm.update({id: 1, name: 'no-name', record_status: 'active'});
+  let role = await rom.update({id: 1, name: 'new name', record_status: 'active'});
 
-  t.deepEqual(data.id, 1);
-  t.deepEqual(data.name, 'no-name');
-  t.deepEqual(data.record_status, 'active');
+  t.deepEqual(role.id, 1);
+  t.deepEqual(role.name, 'new name');
+  t.deepEqual(role.record_status, 'active');
 
-  data = await rm.getRole({id: 1, record_status: 'active'});
+  role = await rom.getRole({id: 1, record_status: 'active'});
 
-  t.deepEqual(data.id, 1);
-  t.deepEqual(data.name, 'no-name');
+  t.deepEqual(role.id, 1);
+  t.deepEqual(role.name, 'new name');
 });
 
 test(`role.model.delete`, async t => {
 
-  let data = await rm.delete({id: 1, record_status: 'active'});
+  let role = await rom.delete({id: 1, record_status: 'active'});
 
-  t.deepEqual(data.id, 1);
-  t.deepEqual(data.name, 'no-name');
+  t.deepEqual(role.id, 1);
+  t.deepEqual(role.name, 'new name');
 
-  data = await rm.getRole({id: 1, record_status: 'deleted'});
+  role = await rom.getRole({id: 1, record_status: 'deleted'});
 
-  t.deepEqual(data, null);
+  t.deepEqual(role, null);
 });
--- components/user/user.model.test.js
+++ components/user/user.model.test.js
@@ -1,60 +1,125 @@
 'use strict';
 
 const test = require('ava'),
-  makeFakeDb = require('../../test/fake-db-sql'),
-  fakeDb = makeFakeDb(),
-  storage = {};
-
-fakeDb.setStorage(storage);
-
-/* replace 'db' cache for './user.model' */
-delete require.cache[require.resolve('../../libs/db-sql')];
-require.cache[require.resolve('../../libs/db-sql')] = {
-  exports: fakeDb,
-};
-
-const um = require('./user.model');
-
-test(`user.model.getUsers`, async t => {
-
-  await um.getUsers({limit: 10, offset: 10});
-  t.deepEqual(storage.select.shift(), '*');
-  t.deepEqual(storage.from.shift(), 'users');
-  t.deepEqual(storage.orderBy.shift(), 'id');
-  t.deepEqual(storage.limit.shift(), 10);
-  t.deepEqual(storage.offset.shift(), 10);
-});
+  rom = require('../role/role.model'),
+  usm = require('./user.model');
 
 test(`user.model.create`, async t => {
 
-  await um.create({firstname: 'firstname'});
-  t.deepEqual(storage.model.shift(), 'users');
-  t.deepEqual(storage.insert.shift().firstname, 'firstname');
-  t.deepEqual(storage.returning.shift(), '*');
+  let role = await rom.create({name: 'role', record_status: 'active'});
+
+  t.deepEqual(role.id, 1);
+  t.deepEqual(role.name, 'role');
+  t.deepEqual(role.record_status, 'active');
+
+  let user = await usm.create({
+
+    role_id: 1,
+    firstname: 'firstname1',
+    middlename: 'middlename1',
+    lastname: 'lastname1',
+    gender: 'male',
+    age: 30,
+    phone: '+79001112233',
+    record_status: 'active'
+  });
+
+  t.deepEqual(user.id, 1);
+  t.deepEqual(user.role_id, 1);
+  t.deepEqual(user.firstname, 'firstname1');
+  t.deepEqual(user.middlename, 'middlename1');
+  t.deepEqual(user.lastname, 'lastname1');
+  t.deepEqual(user.gender, 'male');
+  t.deepEqual(user.age, 30);
+  t.deepEqual(user.phone, '+79001112233');
+  t.deepEqual(user.record_status, 'active');
+
+  user = await usm.create({
+
+    role_id: 1,
+    firstname: 'firstname2',
+    middlename: 'middlename2',
+    lastname: 'lastname2',
+    gender: 'male',
+    age: 30,
+    phone: '+79001112233',
+    record_status: 'active'
+  });
+
+  t.deepEqual(user.firstname, 'firstname2');
+
+  user = await usm.create({
+
+    role_id: 1,
+    firstname: 'firstname3',
+    middlename: 'middlename3',
+    lastname: 'lastname3',
+    gender: 'male',
+    age: 30,
+    phone: '+79001112233',
+    record_status: 'active'
+  });
+
+  t.deepEqual(user.firstname, 'firstname3');
+
+  let err = await t.throwsAsync(_=> usm.create({
+    role_id: 2,
+    firstname: 'firstname',
+    middlename: 'middlename',
+    lastname: 'lastname',
+    gender: 'male',
+    age: 30,
+    phone: '+79001112233',
+    record_status: 'active'
+  }));
+
+  t.deepEqual(err.code, '23503'); // role not present in table
+});
+
+test(`user.model.getUsers`, async t => {
+
+  let user = await usm.getUsers({limit: 10, offset: 0, record_status: 'active'});
+
+  t.deepEqual(user.length, 3);
+  t.deepEqual(user[0].firstname, 'firstname1');
+  t.deepEqual(user[1].firstname, 'firstname2');
+  t.deepEqual(user[2].firstname, 'firstname3');
+
+  user = await usm.getUsers({limit: 1, offset: 1, record_status: 'active'});
+
+  t.deepEqual(user.firstname, 'firstname2');
 });
 
 test(`user.model.getUser`, async t => {
 
-  await um.getUser({id: 1});
-  t.deepEqual(storage.select.shift(), '*');
-  t.deepEqual(storage.from.shift(), 'users');
-  t.deepEqual(storage.where.shift().id, 1);
+  let user = await usm.getUser({id: 1, record_status: 'active'});
+
+  t.deepEqual(user.id, 1);
+  t.deepEqual(user.firstname, 'firstname1');
 });
 
 test(`user.model.update`, async t => {
 
-  await um.update({id: 1, firstname: 'firstname'});
-  t.deepEqual(storage.model.shift(), 'users');
-  t.deepEqual(storage.update.shift().firstname, 'firstname');
-  t.deepEqual(storage.where.shift().id, 1);
-  t.deepEqual(storage.returning.shift(), '*');
+  let user = await usm.update({id: 1, firstname: 'new firstname', record_status: 'active'});
+
+  t.deepEqual(user.id, 1);
+  t.deepEqual(user.firstname, 'new firstname');
+  t.deepEqual(user.record_status, 'active');
+
+  user = await usm.getUser({id: 1, record_status: 'active'});
+
+  t.deepEqual(user.id, 1);
+  t.deepEqual(user.firstname, 'new firstname');
 });
 
 test(`user.model.delete`, async t => {
 
-  await um.delete({id: 1});
-  t.deepEqual(storage.model.shift(), 'users');
-  t.deepEqual(storage.update.shift().hasOwnProperty('record_status'), true);
-  t.deepEqual(storage.where.shift().id, 1);
-  t.deepEqual(storage.returning.shift(), '*');
+  let user = await usm.delete({id: 1, record_status: 'active'});
+
+  t.deepEqual(user.id, 1);
+  t.deepEqual(user.firstname, 'new firstname');
+
+  user = await usm.getUser({id: 1, record_status: 'deleted'});
+
+  t.deepEqual(user, null);
 });
